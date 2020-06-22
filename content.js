5d4e0f648a16956b715c5f1f2bb98af8632113dd
rasmuszimmer <rasmuszimmer@hotmail.com>
2020-06-22 14:38:39 +0200

    add permission

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.message === 'refresh_dashboard') {
         var dashboardFrame = document.querySelector("div.dashboardContainer > iframe");
-        dashboardFrame.contentWindow.document.body.querySelector('.slds-button.slds-button_neutral.refresh').click();
+        var refreshButton = dashboardFrame.contentWindow.document.body.querySelector('.slds-button.slds-button_neutral.refresh');
+        refreshButton.click();
     }
 });
